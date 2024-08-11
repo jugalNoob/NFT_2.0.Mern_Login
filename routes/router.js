56b469401e3046ddadd840396db0ec970037e4a0
jugalNoob <sjugal126@gmail.com>
2024-08-10 21:39:23 -0700

    update

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = new express.Router();
 const controllers=require("../controollers/userControllers")
+const authenticate =require('../middleware/authenticate.js')
 router.get("/home/jugal", controllers.first);
 router.post("/signin",controllers.form )
 router.post("/signup",controllers.login )
+router.get('/Count', authenticate, controllers.auth);
 module.exports = router;
